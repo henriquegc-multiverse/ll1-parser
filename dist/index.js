65c23bc7271f9bc1ce8ae3265bd2eeaf4a3e9b91
Henrique GC <henriq4net@gmail.com>
2024-08-03 22:53:15 -0300

    fix: removing dist folder

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_fs_1 = __importDefault(require("node:fs"));
-const table = node_fs_1.default.readFileSync("./entry.csv", "utf-8").toString();
-console.log(table);
